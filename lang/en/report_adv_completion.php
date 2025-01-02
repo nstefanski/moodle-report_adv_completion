72d0c86c1bb07492e4f8ec7d8d148cf9ff98dc17
Nicholas Stefanski <nmstefanski@gmail.com>
2025-01-02 10:18:36 -0600

    Create report_adv_completion.php

---
+++ lang/en/report_adv_completion.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package    report
+ * @subpackage adv_completion
+ * @author     Nick Stefanski <nmstefanski@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['clicktomarkusercomplete'] = 'Click to mark user complete';
+$string['adv_completion:view'] = 'View course advanced completion report';
+$string['completiondate'] = 'Completion date';
+$string['id'] = 'ID';
+$string['name'] = 'Name';
+$string['grade'] = 'Final Course Grade';
+$string['nocapability'] = 'Can not access user advanced completion report';
+$string['page-report-completion-x'] = 'Any advanced completion report';
+$string['page-report-completion-index'] = 'Course advanced completion report';
+$string['page-report-completion-user'] = 'User course advanced completion report';
+$string['pluginname'] = 'Post Final Grades';
+$string['eventreportviewed'] = 'Advanced completion report viewed';
+$string['eventuserreportviewed'] = 'Advanced completion user report viewed';
+$string['eventgradeposted'] = 'Advanced completion grade posted';
